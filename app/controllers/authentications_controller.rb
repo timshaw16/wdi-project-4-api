8bcadfd825562e36f54a95875717ad195a7c064c
timshaw16 <tshaw2008@icloud.com>
2017-06-02 12:35:13 +0100

    reg not fully working, started on login

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,9 +1,22 @@
 class AuthenticationsController < ApplicationController
   def register
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :ok
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def login
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      render json: user, status: :ok
+    else
+      render json: { errors: ["Invalid login credentials."]}, status: 401
+    end
   end
+
   private
   def user_params
     hash = {}
